67e262a2e4390249d6030423b2c06717fb46a283
Matheus D. Bulho <mabulho@rockwellautomation.com>
2018-04-06 14:14:31 -0400

    v2

---
+++ js/makeBlobs.js
@@ -0,0 +1,46 @@
+class MakeBlobs {
+    constructor(Sprite, gameScene) {
+        let numberOfBlobs = 6,
+            spacing = 48,
+            xOffset = 150,
+            speed = 2,
+            direction = 1;
+
+        //An array to store all the blob monsters
+        let blobs = [];
+
+        //Make as many blobs as there are `numberOfBlobs`
+        for (let i = 0; i < numberOfBlobs; i++) {
+
+            //Make a blob
+            let blob = new Sprite(id["blob.png"]);
+
+            //Space each blob horizontally according to the `spacing` value.
+            //`xOffset` determines the point from the left of the screen
+            //at which the first blob should be added
+            let x = spacing * i + xOffset;
+
+            //Give the blob a random `y` position
+            let y = randomInt(0, gameScene.height - blob.height);
+
+            //Set the blob's position
+            blob.x = x;
+            blob.y = y;
+
+            //Set the blob's vertical velocity. `direction` will be either `1` or
+            //`-1`. `1` means the enemy will move down and `-1` means the blob will
+            //move up. Multiplying `direction` by `speed` determines the blob's
+            //vertical direction
+            blob.vy = speed * direction;
+
+            //Reverse the direction for the next blob
+            direction *= -1;
+
+            //Push the blob into the `blobs` array
+            blobs.push(blob);
+
+            //Add the blob to the `gameScene`
+            gameScene.addChild(blob);
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,46 +1,75 @@
 //Aliases
 let Application = PIXI.Application,
+  Container = PIXI.Container,
   loader = PIXI.loader,
   resources = PIXI.loader.resources,
-  Sprite = PIXI.Sprite;
+  Graphics = PIXI.Graphics,
+  TextureCache = PIXI.utils.TextureCache,
+  Sprite = PIXI.Sprite,
+  Text = PIXI.Text,
+  TextStyle = PIXI.TextStyle;
 
 //Create a Pixi Application
 let app = new Application({
-  width: 560,
-  height: 560,
-  antialias: true,
+  width: 512,
+  height: 512,
+  antialiasing: true,
   transparent: false,
   resolution: 1
 }
 );
-app.renderer.backgroundColor = 0xe0e0e0;
-
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
-
-//load an image and run the `setup` function when it's done
 loader
-  .add("img/block.jpg")
+  .add("img/treasureHunter.json")
   .load(setup);
 
-let block, state;
+let state;
 
 //This `setup` function will run when the image has loaded
 function setup() {
 
-  //Create the block sprite
-  block = new Sprite(loader.resources["img/block.jpg"].texture);
-  block.vx = 0;
-  block.vy = 0;
+  gameScene = new Container();
+  app.stage.addChild(gameScene);
 
-  //assigns keyboard motion handler
-  new KeyboardHandler(block, 2);
+  gameOverScene = new Container();
+  app.stage.addChild(gameOverScene);
+  gameOverScene.visible = false;
+
+  //Create an alias for the texture atlas frame ids
+  id = resources["img/treasureHunter.json"].textures;
+
+  //Dungeon
+  dungeon = new Sprite(id["dungeon.png"]);
+  gameScene.addChild(dungeon);
+
+  //Door
+  door = new Sprite(id["door.png"]);
+  door.position.set(32, 0);
+  gameScene.addChild(door);
+
+  //Explorer
+  explorer = new Sprite(id["explorer.png"]);
+  explorer.x = 68;
+  explorer.y = gameScene.height / 2 - explorer.height / 2;
+  explorer.vx = 0;
+  explorer.vy = 0;
+  gameScene.addChild(explorer);
+
+  //Treasure
+  treasure = new Sprite(id["treasure.png"]);
+  treasure.x = gameScene.width - treasure.width - 48;
+  treasure.y = gameScene.height / 2 - treasure.height / 2;
+  gameScene.addChild(treasure);
+
+  //Make Blobs
+  new MakeBlobs(Sprite,gameScene);
+
+  //Attaches keyboard handler
+  new KeyboardHandler(explorer,2);
 
   //Set the game state
   state = play;
-
-  //Add the block to the stage
-  app.stage.addChild(block);
   app.ticker.add(delta => gameLoop(delta));
 }
 
@@ -51,9 +80,8 @@ function gameLoop(delta) {
 
 function play(delta) {
   //Move
-  block.x += block.vx;
-  block.y += block.vy;
-
-  contain(block, {x: 0, y: 0, width: 488, height: 480});
+  explorer.x += explorer.vx;
+  explorer.y += explorer.vy;
+  contain(explorer, {x: 28, y: 10, width: 488, height: 480});
 }
 
